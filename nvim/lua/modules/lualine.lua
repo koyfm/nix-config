bf38233fb45ea9661d565f91e107bfa908b064a9
Itzik <itzik@koyfman.net>
2026-03-09 10:00:16 +0200

    lualine buffers

--- nvim/lua/modules/lualine.lua
+++ nvim/lua/modules/lualine.lua
@@ -1,5 +1,18 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = {}
+  opts = {
+    options = {
+      -- https://github.com/nvim-lualine/lualine.nvim/issues/1322
+      component_separators = '|',
+    },
+    sections = {
+      lualine_c = {
+        {
+          'buffers',
+          show_filename_only = true
+        }
+      }
+    }
+  }
 }
