1876f1947171329009c1ff42fa7f2441a976970c
Itzik <itzik@koyfman.net>
2026-03-11 00:52:48 +0200

    lsp

--- nvim/lua/modules/lspconfig.lua
+++ nvim/lua/modules/lspconfig.lua
@@ -1,6 +1,15 @@
 return {
   "neovim/nvim-lspconfig",
   config = function()
-    vim.lsp.enable({ "eslint", "lua_ls", "nixd", "ruff", "ty" })
+    vim.lsp.enable({
+      "cssls",
+      "eslint",
+      "jsonls",
+      "lua_ls",
+      "nixd",
+      "ruff",
+      "ty",
+      "yamlls",
+    })
   end,
 }
