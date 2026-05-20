ca4c49052098a4501b6d946388eb554ab8fb0839
Itzik <itzik@koyfman.net>
2026-05-20 19:36:26 +0300

    centaurus boot kernel params

--- modules/flake/configurations/nixos/centaurus/_hardware.nix
+++ modules/flake/configurations/nixos/centaurus/_hardware.nix
@@ -11,9 +11,7 @@
   ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
-  boot.kernelParams = [
-    "amdgpu.ppfeaturemask=0xf7fff" # disable "PP_GFXOFF_MASK" dynamic graphics engine
-  ];
+  boot.kernelParams = [ ];
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/ddb26d08-4077-4342-8bed-d87ba43c6234";
